332fb57c18a92065322575c909f85f9a7b0d97b4
AyaZain <ayaashrafzain@gmail.com>
2023-07-14 02:38:22 +0300

    *

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173/");
+  res.header("Access-Control-Allow-Origin", "*");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
     return res.status(200).json({});
